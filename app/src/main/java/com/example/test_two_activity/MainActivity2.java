37614d4843e3020a1389fe2c71dfc51c61b8c75d
QUAGLIOTOMMASO <tommaso.quaglio@ptpvenezia.edu.it>
2023-12-07 11:32:09 +0100

    finish

--- app/src/main/java/com/example/test_two_activity/MainActivity2.java
+++ app/src/main/java/com/example/test_two_activity/MainActivity2.java
@@ -6,14 +6,15 @@ import android.view.View;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
     }
 
     public void onClick(View view){
-        startActivity(new Intent(MainActivity.this, MainActivity2.class));
+        startActivity(new Intent(MainActivity2.this, MainActivity.class));
     }
 }
